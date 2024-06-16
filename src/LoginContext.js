9893cb4d0009d05a90c7ce5ff72152dd1e364634
PrajapatiVikrant <146848720+PrajapatiVikrant@users.noreply.github.com>
2024-06-16 22:13:08 +0530

    Add files via upload

---
+++ src/LoginContext.js
@@ -0,0 +1,23 @@
+import { createContext, useState } from "react";
+
+const LoginContext = createContext();
+
+const LoginProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = () => {
+    setIsLoggedIn(true);
+  };
+
+  const logout = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <LoginContext.Provider value={{ isLoggedIn, login, logout }}>
+      {children}
+    </LoginContext.Provider>
+  );
+};
+
+export { LoginProvider, LoginContext };
